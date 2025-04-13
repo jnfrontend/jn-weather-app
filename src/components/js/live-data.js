cabb386e289b9de1df30f0221cafd314f4a77d1a
Jarek <jnfrontend@gmail.com>
2025-04-13 18:12:59 +0100

    Feat: 003 - display weather api data in weather card

--- src/components/js/live-data.js
+++ src/components/js/live-data.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded",() => {
 
 	const weatherSearchBar = document.getElementById('searchWeatherByCityName');
 	const weatherSearchButton = document.getElementById('searchWeatherAction');
+    const weatherCardIcon = document.getElementById('weather_card_icon');
 
     // Function to fetch weather data
 	async function fetchWeather(cityName) {
@@ -17,6 +18,26 @@ document.addEventListener("DOMContentLoaded",() => {
 
 		const weatherData = await response.json();
 		console.log(weatherData);
+
+        document.querySelector(".weather_card_city").innerHTML = weatherData.name;
+        document.querySelector(".weather_card_temp").innerHTML = Math.round(weatherData.main.temp) + "<span>&deg;C</span>" ;
+
+        // Display weather icon
+        if(weatherData.weather[0].description == "clear sky") {
+            weatherCardIcon.src = "images/weather-icons/clear.png";
+        } else if(weatherData.weather[0].main == "Clouds") {
+            weatherCardIcon.src = "images/weather-icons/clouds.png";
+        } else if(weatherData.weather[0].description == "shower rain") {
+            weatherCardIcon.src = "images/weather-icons/shower_rain.png";
+        } else if(weatherData.weather[0].description == "rain") {
+            weatherCardIcon.src = "images/weather-icons/drizzle.png";
+        } else if(weatherData.weather[0].description == "thunderstorm") {
+            weatherCardIcon.src = "images/weather-icons/thunderstorm.png";
+        } else if(weatherData.weather[0].description == "mist") {
+            weatherCardIcon.src = "images/weather-icons/mist.png";
+        } else if(weatherData.weather[0].description == "snow") {
+            weatherCardIcon.src = "images/weather-icons/snow.png";
+        }
 	}
 
     // When the user enters the city name and clicks the Search button -> display weather data
