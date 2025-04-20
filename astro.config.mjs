e87684abd871330961a2090859fd4e1da7a3b35b
Jarek <jnfrontend@gmail.com>
2025-04-20 11:40:00 +0100

    Configure astro for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 
+const GH_LIVE_URL = 'https://jnfrontend.github.io';
+const GH_PAGE_NAME = 'jn-weather-app';
+
 // https://astro.build/config
 export default defineConfig({
+    site: GH_LIVE_URL,
+    base: GH_PAGE_NAME,
     env: {
     schema: {
       OPEN_WEATHER_API_URL: envField.string({ context: "client", access: "public" }),
